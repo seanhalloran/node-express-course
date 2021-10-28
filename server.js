1926713b0c01d3d5ab5481617b0cf37e0ed36ee2
Sean <halloran.sp@gmail.com>
2021-10-28 11:36:41 -0400

    add first GET route

--- server.js
+++ server.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const app = express();
-const bodyParser = require('body-parser');
 
-app.use(bodyParser.json())
+app.listen(8000,function(){
+console.log("Server is running")
+}]
 
 const mockUserData=[
 	{name:'Mark'},
@@ -16,38 +17,3 @@ app.get('/users',function(req,res){
 		users: mockUserData
 	})
 })
-// colons are used as variables that be viewed in the params
-app.get('/users/:id',function(req,res){
-	console.log(req.params.id)
-	res.json({
-		success: true,
-		message: 'got one user',
-		user: req.params.id
-	})
-})
-
-app.post('/login',function(req,res){
-	// Typically passwords are encrypted using something like bcrypt before sending to database
-	const username=req.body.username;
-	const password=req.body.password;
-
-	// This should come from the database
-	const mockUsername="billyTheKid";
-	const mockPassword="superSecret";
-
-	if (username===mockUsername && password===mockPassword){
-		// In practice, use JSON web token design method here to make an encrypted token
-		res.json({
-			success: true,
-			message: 'password and username match!',
-			token: 'encrypted token goes here'
-		})
-	} else {
-		res.json({
-			success: false,
-			message: 'password and username do not match'
-		})
-	}
-})
-
-app.listen(8000,function(){console.log('server is listening')})
